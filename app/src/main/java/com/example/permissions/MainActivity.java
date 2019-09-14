7c8abbe68c7ecb730cc84cd5d38ab3035bccefaf
David Schindler <david.schindler@genesys.com>
2019-09-14 17:50:00 -0400

    Adding stuff

--- app/src/main/java/com/example/permissions/MainActivity.java
+++ app/src/main/java/com/example/permissions/MainActivity.java
@@ -9,6 +9,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.util.Log;
 import android.widget.*;
 import android.view.*;
 import android.provider.Settings;
@@ -18,12 +19,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import android.app.Dialog;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.MultiplePermissionsReport;
 import com.karumi.dexter.PermissionToken;
 import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+//import com.google.android.gms.common.GoogleApiAvailability;
 
 import java.util.List;
 
@@ -44,14 +49,17 @@ import java.security.Permissions;
 public class MainActivity extends AppCompatActivity {
 
     private Button btn_grant;
+    private static final String TAG = "Main Activity";
+    private static final int ERROR_DIALOG_REQUEST = 9001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if(ContextCompat.checkSelfPermission(MainActivity.this  , Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
-      && ContextCompat.checkSelfPermission(MainActivity.this  , Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED)  {
+        if(ContextCompat.checkSelfPermission(MainActivity.this , Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+      && ContextCompat.checkSelfPermission(MainActivity.this , Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED && isServicesValid()
+    && ContextCompat.checkSelfPermission(MainActivity.this , Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED)  {
             startActivity(new Intent(MainActivity.this,MapActivity.class));
             finish();
         }
@@ -65,7 +73,8 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v){
              Dexter.withActivity(MainActivity.this) .withPermissions(
                      Manifest.permission.ACCESS_FINE_LOCATION,
-                     Manifest.permission.CAMERA
+                     Manifest.permission.CAMERA,
+                     Manifest.permission.ACCESS_COARSE_LOCATION
              ).withListener(new MultiplePermissionsListener() {
                  @Override public void onPermissionsChecked(MultiplePermissionsReport report) {
                      startActivity(new Intent(MainActivity.this,MapActivity.class));
@@ -78,5 +87,24 @@ public class MainActivity extends AppCompatActivity {
 
          }
         });
+    }
+    public boolean isServicesValid(){
+        Log.d(TAG,"checking google service version");
+       int avaliable = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MainActivity.this);
+       if(avaliable == ConnectionResult.SUCCESS){
+           //everything is ok
+           Log.d(TAG,"Google play services are working");
+           return true;
+       }
+       else if(GoogleApiAvailability.getInstance().isUserResolvableError(avaliable)){
+           Log.d(TAG, "An error occured that can be fixed");
+           Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(MainActivity.this,avaliable,ERROR_DIALOG_REQUEST);
+           dialog.show();
+
+       }
+       else {
+           Toast.makeText(this,"You can't make requests, ", Toast.LENGTH_SHORT).show();
+       }
+        return false;
     }
 }
