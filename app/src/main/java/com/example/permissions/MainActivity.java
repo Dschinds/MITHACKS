9a9b54e4a174afcb2c110af21ee40ac51db78c6c
David Schindler <david.schindler@genesys.com>
2019-09-14 15:31:42 -0400

    Permissions

--- app/src/main/java/com/example/permissions/MainActivity.java
+++ app/src/main/java/com/example/permissions/MainActivity.java
@@ -12,17 +12,32 @@ import android.net.Uri;
 import android.widget.*;
 import android.view.*;
 import android.provider.Settings;
+import android.Manifest;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+
+import java.util.List;
 
 
 import android.os.Bundle;
 
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.PermissionToken;
-import com.karumi.dexter.listener.PermissionDeniedResponse;
-import com.karumi.dexter.listener.PermissionGrantedResponse;
 import com.karumi.dexter.listener.PermissionRequest;
-import com.karumi.dexter.listener.single.PermissionListener;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+import com.karumi.dexter.MultiplePermissionsReport;
 
 import java.security.Permissions;
 
@@ -35,47 +50,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if(ContextCompat.checkSelfPermission(MainActivity.this  , Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+        if(ContextCompat.checkSelfPermission(MainActivity.this  , Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+      && ContextCompat.checkSelfPermission(MainActivity.this  , Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED)  {
             finish();
         }
+
+
         btn_grant = findViewById(R.id.btn_grant);
 
 
         btn_grant.setOnClickListener(new View.OnClickListener(){
          @Override
             public void onClick(View v){
-             Dexter.withActivity(MainActivity.this).withPermission(Manifest.permission.ACCESS_FINE_LOCATION).withListener(new PermissionListener() {
-                 @Override
-                 public void onPermissionGranted(PermissionGrantedResponse response) {
-                   startActivity(new Intent(MainActivity.this,MapActivity.class));
-                   finish();
-                 }
-
-                 @Override
-                 public void onPermissionDenied(PermissionDeniedResponse response) {
-                    if(response.isPermanentlyDenied()){
-                        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                        builder.setTitle("Permission denied").setMessage("Permission is permanently denied go to settings")
-                                .setNegativeButton("cancel",null)
-                                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                                    @Override
-                                    public void onClick(DialogInterface dialog, int which) {
-                                     Intent intent = new Intent();
-                                     intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-                                     intent.setData(Uri.fromParts("package",getPackageName(),null));
-                                    }
-                                }).show();
+             Dexter.withActivity(MainActivity.this) .withPermissions(
+                     Manifest.permission.ACCESS_FINE_LOCATION,
+                     Manifest.permission.CAMERA
+             ).withListener(new MultiplePermissionsListener() {
+                 @Override public void onPermissionsChecked(MultiplePermissionsReport report) {
+                     startActivity(new Intent(MainActivity.this,MapActivity.class));
+                     finish();
                     }
-                    else {
-                        Toast.makeText(MainActivity.this, "Permissions Denied", Toast.LENGTH_SHORT).show();
-                    }
-                 }
-
-                 @Override
-                 public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
-                    token.continuePermissionRequest();
+                 @Override public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
+                     token.continuePermissionRequest();
                  }
              }).check();
+
          }
         });
     }
